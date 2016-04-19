194b5a8d12cd5308b36b9190cb008b902a5fc8de
amessing <amessing@utk.edu>
2016-04-19 14:50:56 -0400

    Beginning of Waypoint Generator using opencv and tkinter

--- Pathfinder-IEEE_General_Platform_Specific/WaypointGen.py
+++ Pathfinder-IEEE_General_Platform_Specific/WaypointGen.py
@@ -1,85 +1,141 @@
-import argparse
 from Tkinter import *
 from tkFileDialog import askopenfilename
-import sys
+from PIL import Image, ImageTk
+import cv2
+import math
 
-courseWidth = 0
-courseLength = 0
-imageFilename = ""
-
-class DimensionDialog:
-
-    def __init__(self, parent):
-
-        self.parent = parent
-
-        top = self.top = Toplevel(parent)
-
-        self.widthLabel = Label(top, text="Course Width (in):")
-        self.widthLabel.pack(padx=5, pady=5)
-        self.widthEntry = Entry(top)
-        self.widthEntry.pack(padx=5, pady=5)
-
-        self.lengthLabel = Label(top, text="Course Length (in):")
-        self.lengthLabel.pack(padx=5, pady=5)
-        self.lengthEntry = Entry(top)
-        self.lengthEntry.pack(padx=5, pady=5)
-
-        self.open = Button(top, text="Open Image File", command=self.open)
-        self.open.pack(padx=5, pady=5)
-
-        self.openLabel = Label(top, text="")
-        self.openLabel.pack(padx=5, pady=5)
-
-        self.save = Button(top, text="Save", command=self.save)
-        self.save.pack(padx=5, pady=5)
-
-    def open(self):
-        global imageFilename
-        imageFilename = askopenfilename(parent=self.parent)
-        self.openLabel['text'] = imageFilename
-
-    def save(self):
-        global courseWidth, courseLength
-        courseWidth = self.widthEntry.get()
-        print "Course width (in):", courseWidth
-        courseLength = self.lengthEntry.get()
-        print "Course length (in):", courseLength
-        self.top.destroy()
+class Waypoint:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.theta = 0.0
 
 class Application(Frame):
-    def setBackgroundImage(self, filename):
+    def __init__(self, master=None):
+        self.courseWidth = 0
+        self.courseLength = 0
+        self.imageFilename = ""
         self.waypoints = []
+        self.root = master
+        Frame.__init__(self, master)
+        # width x height + x_offset + y_offset:
+        self.root.geometry("175x400+30+30")
+        self.createWidgets()
 
-    def clear(self):
-        self.waypoints = []
+        self.opencv_image = None
+        self.opencv_image_marked = None
+        self.getting_theta = False
 
     def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"]   = "red"
-        self.QUIT["command"] =  self.quit
+        self.image = Label(self.root)
+        self.image.grid(row=0, column=0)
+        self.widthLabel = Label(self.root, text="Course Width (in):")
+        self.widthLabel.grid(row=0, column=1)
+        self.widthEntry = Entry(self.root)
+        self.widthEntry.grid(row=1, column=1)
+        self.lengthLabel = Label(self.root, text="Course Length (in):")
+        self.lengthLabel.grid(row=2,column=1)
+        self.lengthEntry = Entry(self.root)
+        self.lengthEntry.grid(row=3,column=1)
+        self.open = Button(self.root, text="Open Image File", command=self.open)
+        self.open.grid(row=4,column=1)
+        self.openLabel = Label(self.root, text="")
+        self.openLabel.grid(row=5, column=1)
+        self.clearImage = Button(self.root, text="Clear Image", command=self.clearImage)
+        self.clearImage.grid(row=6, column=1)
+        self.clearWaypoints = Button(self.root, text="Clear Waypoint", command=self.clearWaypoints)
+        self.clearWaypoints.grid(row=7, column=1)
+        self.quit = Button(self.root, text="Quit", fg="red", command=self.quit)
+        self.quit.grid(row=8, column=1)
 
-        self.QUIT.pack({"side": "right"})
+    def open(self):
+        imageFilename = askopenfilename(parent=self.root)
+        if(imageFilename != ""):
+            dot = imageFilename.find(".")
+            extension = imageFilename[dot:]
+            if extension in ['.jpg', '.jpeg', '.png', '.tiff']:
+                self.opencv_image = cv2.imread(imageFilename)
+                self.opencv_image_marked = self.opencv_image.copy()
+                self.setImage()
+                imageFilename = imageFilename.rsplit('/', 1)[-1]
+                self.openLabel['text'] = imageFilename
 
-        self.CLEAR = Button(self)
-        self.CLEAR["text"] = "Clear",
-        self.CLEAR["command"] = self.clear
+    def setImage(self):
+        image = Image.fromarray(cv2.cvtColor(self.opencv_image_marked, cv2.COLOR_BGR2RGB))
+        photoimage = ImageTk.PhotoImage(image)
+        self.image = Label(self.root, image=photoimage)
+        self.image.image = photoimage
+        self.image.grid(row=0, column=0, rowspan=25)
+        self.image.bind('<Button-1>', self.mouse_click)
+        (width, height) = image.size
+        self.root.geometry("%dx%d+30+30" % (width + 175, height))
+        #self.moveWidgets(width+25)
 
-        self.CLEAR.pack({"side": "right"})
+    def clearImage(self):
+        self.openLabel['text'] = ""
+        self.image.destroy()
+        self.root.geometry("175x400+30+30")
+        #self.moveWidgets(25)
 
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        img = ImageTk.PhotoImage(Image.open(image_file))
-        panel = tk.Label(root, image = img)
-        panel.pack(side = "bottom", fill = "both", expand = "yes")
-        self.pack()
-        self.createWidgets()
+    def mouse_click(self, event):
+        if not self.getting_theta:
+            cv2.circle(self.opencv_image_marked, (event.x, event.y), 3, (0, 255, 0), 2)
+            self.setImage()
+            self.waypoints.append(Waypoint(event.x,event.y))
+            height, width, channels = self.opencv_image_marked.shape
+
+            if not hasattr(self, 'saveLabel'):
+                self.saveTheta = Button(self.root, text="Save Heading", command=self.saveTheta)
+                self.thetaLabel = Label(self.root, text="Heading (degrees):")
+                self.thetaEntry = Entry(self.root)
+
+            print self.saveTheta['command']
+
+            self.thetaLabel.grid(row=15, column=1)
+            self.thetaEntry.grid(row=16, column=1)
+            self.saveTheta.grid(row=17, column=1)
+
+            self.getting_theta = True
+
+    def saveTheta(self):
+        def is_number(s):
+            try:
+                float(s)
+                return True
+            except ValueError:
+                return False
+
+        theta = self.thetaEntry.get()
+        if(is_number(theta)):
+            self.waypoints[len(self.waypoints)-1].theta = float(theta)
+
+            self.thetaLabel.grid_forget()
+            self.thetaEntry.grid_forget()
+            self.saveTheta.grid_forget()
+
+            p1 = (self.waypoints[len(self.waypoints)-1].x, self.waypoints[len(self.waypoints)-1].y)
+            line_length = 15
+            p2 = (int(p1[0] + line_length * math.cos(math.radians(float(theta)))), int(p1[1] + line_length * math.sin(math.radians(float(theta)))))
+            cv2.line(self.opencv_image_marked, p1, p2,  (0, 0, 255), 2)
+            cv2.putText(self.opencv_image_marked, "%d" % len(self.waypoints), (p1[0], p1[1] - 10), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 0, 0))
+            self.setImage()
+            self.getting_theta = False
+        else:
+            print "Heading must be a number"
+
+    def clearWaypoints(self):
+        self.waypoints = []
+        self.opencv_image_marked = self.opencv_image.copy()
+        self.setImage()
+        if hasattr(self, 'thetaLabel'):
+            self.thetaLabel.grid_forget()
+            self.thetaEntry.grid_forget()
+            self.saveTheta.grid_forget()
+
+
 
 root = Tk()
 root.title("Waypoint Generator")
-d = DimensionDialog(root)
-root.wait_window(d.top)
-app = Application( master=root)
+app = Application(master=root)
 app.mainloop()
 root.destroy()
